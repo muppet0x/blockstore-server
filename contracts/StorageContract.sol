fe5d47859b3a571ab0860e0ba73d83627a5983c2
muppet0x <163330499+muppet0x@users.noreply.github.com>
2024-05-03 23:02:52 +0200

    clean code

--- contracts/StorageContract.sol
+++ contracts/StorageContract.sol
@@ -1,17 +1,22 @@
 pragma solidity ^0.8.0;
 
 contract StorageContract {
-
-    event FileStored(address indexed owner, bytes32 indexed fileHash, string metadata, uint256 timestamp);
-
-    event FileVerified(address indexed verifier, bytes32 indexed fileHash, bool exists);
-
+    event FileStored(
+        address indexed owner,
+        bytes32 indexed fileHash,
+        string metadata,
+        uint256 timestamp
+    );
+    event FileVerified(
+        address indexed verifier,
+        bytes32 indexed fileHash,
+        bool exists
+    );
     struct FileDetails {
         address owner;
         uint256 timestamp;
         string metadata;
     }
-
     mapping(bytes32 => FileDetails) private files;
 
     function storeFileHash(bytes32 fileHash, string memory metadata) public {
@@ -20,7 +25,15 @@ contract StorageContract {
         emit FileStored(msg.sender, fileHash, metadata, block.timestamp);
     }
 
-    function retrieveFileDetails(bytes32 fileHash) public view returns (address owner, uint256 timestamp, string memory metadata) {
+    function retrieveFileDetails(bytes32 fileHash)
+        public
+        view
+        returns (
+            address owner,
+            uint256 timestamp,
+            string memory metadata
+        )
+    {
         require(files[fileHash].timestamp != 0, "File hash not found.");
         FileDetails memory details = files[fileHash];
         return (details.owner, details.timestamp, details.metadata);
