239659bf8fe205bb32b274976c90249e802ab6fd
muppet0x <163330499+muppet0x@users.noreply.github.com>
2024-05-08 17:28:23 +0200

    update with new function

--- contracts/StorageContract.sol
+++ contracts/StorageContract.sol
@@ -8,6 +8,13 @@ contract StorageContract {
         uint256 timestamp
     );
 
+    event FileUpdated(
+        address indexed owner,
+        bytes32 indexed fileHash,
+        string newMetadata,
+        uint256 timestamp
+    );
+
     event FileVerified(
         address indexed verifier,
         bytes32 indexed fileHash,
@@ -26,6 +33,14 @@ contract StorageContract {
         require(files[fileHash].timestamp == 0, "File hash already stored.");
         files[fileHash] = FileDetails(msg.sender, block.timestamp, metadata);
         emit FileStored(msg.sender, fileHash, metadata, block.timestamp);
+    }
+
+    function updateFileMetadata(bytes32 fileHash, string memory newMetadata) public {
+        require(files[fileHash].timestamp != 0, "File hash not stored.");
+        FileDetails storage file = files[fileHash];
+        require(msg.sender == file.owner, "Only the owner can update metadata.");
+        file.metadata = newMetadata;
+        emit FileUpdated(msg.sender, fileHash, newMetadata, block.timestamp);
     }
 
     function retrieveFileDetails(bytes32 fileHash) public view 
